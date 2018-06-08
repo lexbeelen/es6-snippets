eaa4f5092a74bf7fc792140b6a22ed28bcfbd655
Lex Beelen <lex@lexbeelen.nl>
2018-06-08 22:13:51 +0200

    Add new every example

--- ArrayHelperMethods/every.js
+++ ArrayHelperMethods/every.js
@@ -11,7 +11,7 @@ let allComputersCanRunProgram = computers.every(function (computer) {
 
 console.log(allComputersCanRunProgram);
 
-/* ECMAScript 2016 (ES6) - Example 1 */
+/* ECMAScript 2016 (ES6) - Example 2 */
 let names = [
     "Lex",
     "Lauren",
@@ -23,3 +23,28 @@ let allNamesGreaterThanTwo = names.every(function (name) {
 });
 
 console.log(allNamesGreaterThanTwo);
+
+/* ECMAScript 2016 (ES6) - Example 3 */
+function Field(value) {
+    this.value = value;
+}
+
+Field.prototype.validate = function () {
+    return this.value.length > 0;
+};
+
+let username = new Field("2cool");
+let password = new Field("my_password");
+let birthdate = new Field("10/10/2010");
+
+let fields = [username, password, birthdate];
+
+let formIsValid = fields.every(function (field) {
+   return field.validate();
+});
+
+if(formIsValid) {
+    console.log('Success')
+} else {
+    console.log('Error: Invalid')
+}
